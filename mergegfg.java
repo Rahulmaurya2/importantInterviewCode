c6866c7ba0a6ed672a69b2d38ccf35c48590feb6
Rahul Kumar Maurya <69010150+Rahulmaurya2@users.noreply.github.com>
2023-01-20 12:48:53 +0530

    Create  mergegfg.java

---
+++ mergegfg.java
@@ -0,0 +1,38 @@
+//Merge Without Extra Space
+/*Given two sorted arrays arr1[] and arr2[] of sizes n and m in non-decreasing order. Merge them in sorted order without using any extra space. Modify arr1 so that it contains the first N elements and modify arr2 so that it contains the last M elements.
+ 
+
+Example 1:
+
+Input: 
+n = 4, arr1[] = [1 3 5 7] 
+m = 5, arr2[] = [0 2 6 8 9]
+Output: 
+arr1[] = [0 1 2 3]
+arr2[] = [5 6 7 8 9]
+Explanation:
+After merging the two 
+non-decreasing arrays, we get, 
+0 1 2 3 5 6 7 8 9.*/
+
+
+//solution:
+
+class Solution
+{
+    public static void merge(long arr1[], long arr2[], int n, int m) 
+    {
+      int i = 0;
+      long temp = 0;
+ 
+        while (arr1[n - 1] > arr2[0]) {
+            if (arr1[i] > arr2[0]) {
+                temp = arr1[i];
+                arr1[i] = arr2[0];
+                arr2[0] = temp;
+                Arrays.sort(arr2);
+            }
+            i++;
+        }
+    }
+}
