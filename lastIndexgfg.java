fa931fb3b9d72dc841f88ef7022c0e726c0c3ea6
Rahul Kumar Maurya <69010150+Rahulmaurya2@users.noreply.github.com>
2023-01-22 15:39:40 +0530

    Create lastIndexgfg.java

---
+++ lastIndexgfg.java
@@ -0,0 +1,25 @@
+/*
+Given a string S consisting only '0's and '1's,  find the last index of the '1' present in it.
+
+ 
+
+Example 1:
+
+Input:
+S = 00001
+Output:
+4
+Explanation:
+Last index of  1 in given string is 4.
+*/
+//solution
+
+class Solution {
+    public int lastIndex( String s) {
+     
+    for(int i=s.length()-1; i>=0; i--){
+        if(s.charAt(i)=='1') return i;
+    } 
+    return -1;
+    }
+}
